e13ea965be4e9f0e3dbad8e0989abc5cb152d95e
eduponce69 <124503216+eduponce69@users.noreply.github.com>
2023-02-06 15:32:25 -0300

    Proyecto CV
    Proyecto Final: Crear un CV con html, JavaScript y css

---
+++ codigoJS.js
@@ -0,0 +1,55 @@
+/* En este script están las funciones que controlan la visibilidad de determinado texto, haciendo click en botones */
+
+/*En esta primera parte están las funciones que hacen visible a un determinado bloque*/
+
+function mostrar_estudios(){
+
+  document.getElementById('estudios_oculto').style.display = 'block';
+}
+
+function mostrar_tareas1(){
+
+  document.getElementById('experiencia_oculto1').style.display = 'block';
+}
+
+function mostrar_tareas2(){
+
+  document.getElementById('experiencia_oculto2').style.display = 'block';
+}
+
+function mostrar_tareas3(){
+
+  document.getElementById('experiencia_oculto3').style.display = 'block';
+}
+
+function mostrar_tareas4(){
+
+  document.getElementById('experiencia_oculto4').style.display = 'block';
+} 
+
+/* Ahora vienen las funciones que ocultan el bloque desplegado*/
+
+function ocultar_estudios(){
+
+  document.getElementById('estudios_oculto').style.display = 'none';
+}
+
+function ocultar_tareas1(){
+
+  document.getElementById('experiencia_oculto1').style.display = 'none';
+}
+
+function ocultar_tareas2(){
+
+  document.getElementById('experiencia_oculto2').style.display = 'none';
+}
+
+function ocultar_tareas3(){
+
+  document.getElementById('experiencia_oculto3').style.display = 'none';
+}
+
+function ocultar_tareas4(){
+
+  document.getElementById('experiencia_oculto4').style.display = 'none';
+} 
